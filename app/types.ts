023e6e6b447051dfe566e46923828fd5dd58d2c8
Eric Xiao <ericxiao0000@gmail.com>
2026-01-17 16:11:49 -0800

    Add live class type

--- app/types.ts
+++ app/types.ts
@@ -7,6 +7,7 @@ export interface LiveClass {
 	durationMinutes: number;
 	capacity: number;
 	sneakScore: "High" | "Medium" | "Low";
+	type: "Lecture" | "Seminar" | "Lab";
 }
 
 export interface Course {

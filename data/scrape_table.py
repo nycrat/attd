356bc5e3cea19023ab42fd011eb611aa8dfd079b
Eric Xiao <ericxiao0000@gmail.com>
2026-01-17 22:48:30 -0800

    Update scraper

--- data/scrape_table.py
+++ data/scrape_table.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
+import sys
 
-with open("table.html", "r", encoding="utf-8") as f:
+with open(sys.argv[1], "r", encoding="utf-8") as f:
     html = f.read()
 
 soup = BeautifulSoup(html, "html.parser")
@@ -14,16 +15,18 @@ for table in soup.find_all("table"):
 if len(top_level_tables) < 2:
     raise ValueError("Less than 2 top-level tables found")
 
+location = soup.find("span", class_="header-0-1-1").get_text(strip=True)
+capacity = soup.find("span", class_="header-2-0-1").get_text(strip=True)
+
 schedule_table = top_level_tables[1]
 
-
-def print_course_and_instructor(cell):
+def course_and_instructor(cell):
     tables = cell.find_all("table", class_="object-cell-args")
 
     courses = tables[0].get_text(strip=True)
     instructor = tables[1].find("td", align="left").get_text(strip=True)
 
-    print(courses, instructor)
+    return (courses, instructor)
 
 
 schedule_matrix_delay = [0, 0, 0, 0, 0, 0, 0, 0]
@@ -48,13 +51,19 @@ for tr in schedule_table.find_all("tr")[1:]:
             i += 1
             continue
 
-        if tds[i].get("class")[0] in ["LEC", "DIS", "SEM"]:
+        class_type = tds[i].get("class")[0]
+        if class_type in ["LEC", "DIS", "SEM", "LAB"]:
             duration = int(tds[i].get("rowspan"))
             schedule_matrix_delay[current_day] = duration - 1
-            print(
-                f"Time: {current_time} | Duration: {duration * 30} | Day : {current_day}")
-            print_course_and_instructor(tds[i])
-            print()
+            course, instructor = course_and_instructor(tds[i])
+
+            translate = {"LEC": "Lecture", "DIS": "Discussion", "SEM": "Seminar", "LAB": "Lab"}
+            course = course.split("-")[0]
+            course = course.replace("_V", "")
+
+            instructor = instructor.replace("'", "''")
+
+            print(f"(DEFAULT, '{course}', '{instructor}', '{location}', '{current_time}', {duration * 30}, {capacity}, 'Low', '{translate[class_type]}', {current_day}),")
 
         i += 1
         current_day += 1
